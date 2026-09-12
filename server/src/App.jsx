f13f9c224b459e81ca3ea4f0061aec76f414e033
beckarama <bargasbeck@gmail.com>
2026-09-12 12:06:46 -0500

    feat: add camera module for desktop

--- server/src/App.jsx
+++ server/src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import Feed from "./pages/Feed";
 import Map from "./pages/Map";
 import BottomTabBar from "./components/BottomTabBar";
+import Camera from "./pages/Camera";
+import Result from "./pages/Result";
 import "./App.css";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="/map" element={<Map />} />
+        <Route path="/camera" element={<Camera />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
 
       <BottomTabBar />
