9549499d89c0855326822f23d867a709c9f2b3a7
beckarama <bargasbeck@gmail.com>
2026-09-12 15:37:54 -0500

    feat: add bookmarks as pages and notebook style webpage

--- server/src/App.jsx
+++ server/src/App.jsx
@@ -2,28 +2,30 @@ import { Routes, Route, useLocation } from "react-router";
 import Home from "./pages/Home";
 import Feed from "./pages/Feed";
 import Map from "./pages/Map";
-import BottomTabBar from "./components/BottomTabBar";
+import JournalShell from "./components/JournalShell";
 import Camera from "./pages/Camera";
 import Result from "./pages/Result";
 import "./App.css";
 
 function App() {
   const location = useLocation();
-  const hideTabBar = ["/camera", "/result"].includes(location.pathname);
 
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/camera" element={<Camera />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
-
-      {!hideTabBar && <BottomTabBar />}
-    </>
+  const routes = (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/feed" element={<Feed />} />
+      <Route path="/map" element={<Map />} />
+      <Route path="/camera" element={<Camera />} />
+      <Route path="/result" element={<Result />} />
+    </Routes>
   );
+
+  // the viewfinder is full-bleed; every other route is a page of the journal
+  if (location.pathname === "/camera") {
+    return routes;
+  }
+
+  return <JournalShell>{routes}</JournalShell>;
 }
 
 export default App;
